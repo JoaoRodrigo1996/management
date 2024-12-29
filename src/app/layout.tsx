fd72b591bb98228c7097d66d66c4eb993c510a92
Rodrigo Mesquita <dev.rodrigomesquita@gmail.com>
2024-12-28 22:52:56 -0300

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,10 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      >        
+        <div className="max-w-4xl mx-auto">
+          {children}
+        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,42 @@
-import Image from "next/image";
+import { Search } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <header>
+        Gerenciamento de Funcionários
+      </header>
+      <div className="space-y-6 mt-8">
+        <div className='flex items-center gap-3 px-4 w-72 py-1.5 border border-white/10 rounded-lg' >
+          <input 
+            className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0" 
+            placeholder="Buscar funcionário..." 
+          />
+          <Search className='size-4 ' />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <section className="border border-white/10 rounded">
+          <table className="w-full">
+            <thead className="border-b border-white/10">
+              <tr>
+                <th className="py-3 px-4 text-sm font-semibold text-left">Nome</th>
+                <th className="py-3 px-4 text-sm font-semibold text-left">Email</th>
+                <th className="py-3 px-4 text-sm font-semibold text-left">Telefone</th>
+                <th className="py-3 px-4 text-sm font-semibold text-left">Salário</th>
+                <th className="py-3 px-4 text-sm font-semibold text-left">Data</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="py-3 px-4 text-sm text-zinc-200">Rodrigo Mesquita</td>
+                <td className="py-3 px-4 text-sm text-zinc-200">dev.rodrigomesquita@gmail.com</td>
+                <td className="py-3 px-4 text-sm text-zinc-200">55+ (24) 9 99876 9562</td>
+                <td className="py-3 px-4 text-sm text-zinc-200">R$ 8.000,00</td>
+                <td className="py-3 px-4 text-sm text-zinc-200">28/12/2024</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </div>
+    </>
   );
 }
