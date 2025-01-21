bb95d6c43460fb7fda75418d555b1e7d2d792b36
Rodrigo Mesquita <dev.rodrigomesquita@gmail.com>
2025-01-21 10:57:56 -0300

    refact: add try/catch block when fetching data from database

--- src/actions/get-employers/index.ts
+++ src/actions/get-employers/index.ts
@@ -6,23 +6,28 @@ import { network } from "@/utils/network";
 export async function getEmployers(page: number, filter?: { q?: string }) {
   await network(2000)
 
-  const [employers, total] = await Promise.all([
-    prisma.employer.findMany({
-      take: 10,
-      skip: (page - 1) * 10,
-      orderBy: {
-        name: 'asc'
-      },
-      where: {
-        AND: [
-          filter?.q ? {
-            OR: [{ name: { contains: filter.q } }, { job: { contains: filter.q } }]
-          } : {},
-        ]
-      }
-    }),
-    prisma.employer.count()
-  ])
-
-  return { employers, total }
+  try {
+    const [employers, total] = await Promise.all([
+      prisma.employer.findMany({
+        take: 10,
+        skip: (page - 1) * 10,
+        orderBy: {
+          name: 'asc'
+        },
+        where: {
+          AND: [
+            filter?.q ? {
+              OR: [{ name: { contains: filter.q } }, { job: { contains: filter.q } }]
+            } : {},
+          ]
+        }
+      }),
+      prisma.employer.count()
+    ])
+  
+    return { employers, total }
+  } catch (error) {
+    console.error(error)
+    return { employers: [], total: 0 }
+  }
 }
